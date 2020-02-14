4f6fc917ea4152ba50aea4e1ed0d949f29d201b6
Tim Huddle <tim.huddle@cardinalhealth.com>
2020-02-14 14:54:56 -0500

    pre-stage-2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css'
 function Square (props) {
   return (
     <button className='square'>
-      {/* TODO */}
+      {props.value}
     </button>
   )
 }
@@ -17,19 +17,19 @@ function Board () {
     <div>
       <div className='status'>{status}</div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
       </div>
     </div>
   )
