c7357e11eaa8884b53419d03c6e537dfb2af49b2
Tim Huddle <tim.huddle@cardinalhealth.com>
2020-02-14 16:14:50 -0500

    post-stage-4

--- src/index.js
+++ src/index.js
@@ -12,10 +12,12 @@ function Square (props) {
 
 function Board () {
   const [squares, setSquares] = useState([])
+  const winner = calculateWinner(squares)
   const [currentPlayer, setCurrentPlayer] = useState('X')
-  const status = `Next player: ${currentPlayer}`
+  const status = winner ? `Winner: ${winner}` : `Next player: ${currentPlayer}`
 
   function handleClick (i) {
+    if (calculateWinner(squares) || squares[i]) return
     const newSquares = squares.slice()
     newSquares[i] = currentPlayer
     setSquares(newSquares)
@@ -63,3 +65,25 @@ function Game () {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById('root'))
+
+function calculateWinner (squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+
+  return null
+}
